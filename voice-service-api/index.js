fb91eb49081f0d601787a64db7fa59a86ac9001a
kyochan <guts.kyo.ca@gmail.com>
2023-01-06 11:03:09 -0500

    write to blob storage

--- voice-service-api/index.js
+++ voice-service-api/index.js
@@ -1,57 +1,90 @@
+const azure = require("azure-storage");
+const sdk = require("microsoft-cognitiveservices-speech-sdk");
+
 module.exports = async function(context, req) {
   // <code>
   "use strict";
+  const key = process.env.TEXT_TO_SPEECH_API_KEY;
+  const blobEndpoint = process.env.AUDIO_BLOB_STORAGE_ENDPOINT;
+  const text = (req.query.text || (req.body && req.body.text));
+  // const file = (req.query.filename || (req.body && req.body.filename));
+  const blobConnString = process.env.AUDIO_BLOB_STORAGE_CONNECTION_STRING;
+  const audioFileName = `torstar_${new Date().getTime()}.wav`;
+
+  if (typeof text === "undefined") {
+    context.res = {
+      body: "please input \"text\"",
+    };
+    return;
+  }
 
   // pull in the required packages.
-  var sdk = require("microsoft-cognitiveservices-speech-sdk");
+
+  const blobService = azure.createBlobService(blobConnString);
+  const writableStream = blobService.createWriteStreamToBlockBlob(
+    "audiofiles",
+    audioFileName,
+    {
+      blockIdPrefix: "block",
+      contentSettings: {
+        contentType: "audio/wav",
+      },
+    },
+  );
   context.log("JavaScript HTTP trigger function processed a request.");
 
-  const key = (req.query.key || (req.body && req.body.key));
-  const file = (req.query.file || (req.body && req.body.file));
+
   var subscriptionKey = key;
   var serviceRegion = "eastus"; // e.g., "westus"
-  var filename = "torstar-news";
 
   // we are done with the setup
 
   // now create the audio-config pointing to our stream and
   // the speech config specifying the language.
-  var audioConfig = sdk.AudioConfig.fromAudioFileOutput(`${filename}_${new Date().getTime()}.wav`);
   var speechConfig = sdk.SpeechConfig.fromSubscription(subscriptionKey, serviceRegion);
 
   // create the speech synthesizer.
-  var synthesizer = new sdk.SpeechSynthesizer(speechConfig, audioConfig);
+  var synthesizer = new sdk.SpeechSynthesizer(speechConfig);
 
   // start the synthesizer and wait for a result.
-  var startTime = new Date().getTime()
-  const text = require(`./${file}.js`);
-  synthesizer.speakTextAsync(text,
-    function(result) {
-      if (result.reason === sdk.ResultReason.SynthesizingAudioCompleted) {
-        console.log("synthesis finished.");
-      } else {
-        console.error("Speech synthesis canceled, " + result.errorDetails +
-          "\nDid you update the subscription info?");
-      }
-      synthesizer.close();
-      synthesizer = undefined;
-      console.log(`>> file: ${file}, character length: ${text.length}`);
-      console.log(`>> file: ${file}, time to process: ${Math.ceil((new Date().getTime() - startTime) / 1000)}`);
-      console.log("Now synthesizing to: " + filename);
+  var startTime = new Date().getTime();
+  const translation = text;
+  await new Promise((resolve) => {
+    synthesizer.speakTextAsync(text,
+      function(result) {
+        if (result.reason === sdk.ResultReason.SynthesizingAudioCompleted) {
+          console.log("synthesis finished.");
+        } else {
+          console.error("Speech synthesis canceled, " + result.errorDetails +
+            "\nDid you update the subscription info?");
+        }
 
-      context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: `>> file: ${file}, character length: ${text.length}\n
-        >> file: ${file}, time to process: ${Math.ceil((new Date().getTime() - startTime) / 1000)}`,
-      };
-    },
-    function(err) {
-      console.trace("err - " + err);
-      synthesizer.close();
-      synthesizer = undefined;
-      context.res = {
-        status: 400,
-        body: "error: " + e.toString(),
-      };
-    });
+        writableStream.end(Buffer.from(result.audioData), () => {
+          synthesizer.close();
+
+          synthesizer = undefined;
+
+          context.res = {
+            body: {
+              stats: {
+                processTimeInSeconds: Math.ceil((new Date().getTime() - startTime) / 1000),
+                numOfCharacters: translation.length,
+              },
+              audioFile: `${blobEndpoint}/${audioFileName}`,
+            },
+          };
+          resolve();
+        });
+      },
+      function(err) {
+        console.trace("err - " + err);
+        synthesizer.close();
+        synthesizer = undefined;
+        context.res = {
+          status: 400,
+          body: "error: " + err.toString(),
+        };
+        resolve();
+      });
+  });
 };
